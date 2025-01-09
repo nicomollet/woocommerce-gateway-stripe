ab2095debe11e7449e05fb48b168a1d4893320c4
Wesley Rosa <wesleyjrosa@gmail.com>
2025-01-09 17:30:55 -0300

    Updating tested up versions

--- woocommerce-gateway-stripe.php
+++ woocommerce-gateway-stripe.php
@@ -10,7 +10,7 @@
  * Requires at least: 6.5
  * Tested up to: 6.7
  * WC requires at least: 9.2
- * WC tested up to: 9.4
+ * WC tested up to: 9.5
  * Text Domain: woocommerce-gateway-stripe
  * Domain Path: /languages
  */
