5da7776f55a7efe30fc3634c6f36927c3bf95a82
Nico Mollet <nico.mollet@gmail.com>
2025-01-15 20:53:20 +0100

    @wjrosa feedback

--- includes/class-wc-gateway-stripe.php
+++ includes/class-wc-gateway-stripe.php
@@ -418,7 +418,7 @@ class WC_Gateway_Stripe extends WC_Stripe_Payment_Gateway {
 			} else {
 				$user_id = get_current_user_id();
 				// Prepare source for user from order _customer_user meta, when no user is logged-in and when on Pay for Order page.
-				if ( 0 === $user_id && is_wc_endpoint_url( 'order-pay' ) ) {
+				if ( ! $user_id && is_wc_endpoint_url( 'order-pay' ) ) {
 					$user_id = $order->get_user_id();
 				}
 				$prepared_source = $this->prepare_source( $user_id, $force_save_source, $stripe_customer_id );
